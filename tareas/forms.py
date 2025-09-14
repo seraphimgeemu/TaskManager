50f32ba67c17c5daf2f1c5bf71700825e64cfad5
SeraphimCode <seraphimgeemu@gmail.com>
2025-09-13 22:56:23 -0400

    1.1

--- tareas/forms.py
+++ tareas/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import Tarea   
+from .models import Tarea 
+from django import forms  
 
 class TareaForm(ModelForm):
     class Meta:
         model = Tarea
         fields = ['title', 'description', 'important']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4, 'class': 'form-control' , 'placeholder': 'Describe la tarea...'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título de la tarea...'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input'})}
